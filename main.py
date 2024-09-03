2f1a82730738b8d400b71fd2e4672590f50e09b1
Marcel Berkholz <code@berkholz.org>
2024-09-03 13:14:32 +0200

    add main script

---
+++ main.py
@@ -0,0 +1,10 @@
+import Crawler.crawler
+import Db
+import Db.database
+
+
+# crawl every module stored in modules path and get application information
+application_links = Crawler.crawler.getApplications("Crawler")
+
+Db.database.init_db()
+Db.database.append_software(application_links)
