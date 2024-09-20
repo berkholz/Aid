8d6ff330f131ad498efb19732b0b0467dd1ca358
Marcel Berkholz <code@berkholz.org>
2024-09-20 16:43:39 +0200

    set path to generic path

--- verify_pkg.py
+++ verify_pkg.py
@@ -73,6 +73,6 @@ def verify_gpg_signature(pkg_path):
 
 
 if __name__ == "__main__":
-    path = "C:/Users/***REMOVED***/Desktop/tmp_package/AID_PACKAGE-24_09_18-Mittwoch"
+    path = "./tmp_package/"
 
     print(verify_pkg_path(path))
