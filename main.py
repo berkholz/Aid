56f385830577acfebe95f7c78873bc7d21045e3e
Marcel Berkholz <code@berkholz.org>
2024-09-30 13:53:32 +0200

    - add function for activating downloads - add function to get all software marked for download from db - add function to download the software

--- main.py
+++ main.py
@@ -9,4 +9,13 @@ application_links = Crawler.crawler.getApplications("Crawler")
 Db.database.init_db()
 Db.database.append_software(application_links)
 
-# downloader.download('win64')
+# activate all software for download which was found latest
+activate_download_for_latest_found()
+
+# reset all download flags
+# reset_download_flag_for_all()
+
+#get software list which have to be downloaded
+software2download = Db.database.get_url_bin_of_software_to_download()
+
+downloader_new.download_software(software2download)
