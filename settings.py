36d5cbb3131070b8b49bae7ac402c834fe09ee7f
Marcel Berkholz <code@berkholz.org>
2024-11-15 00:18:38 +0100

    - add current working directory - add DOWNLOAD_PATH - add global Loglevel

--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
 import logging
+import os
 
-LogLevel = logging.INFO
+LogLevel = logging.DEBUG
+
+cwd_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DOWNLOAD_PATH = os.path.join(cwd_dir, 'downloads/')
