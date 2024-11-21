87d58698ccfa2283de9cb7e75e785fcdde96abbc
Marcel Berkholz <code@berkholz.org>
2024-11-21 15:18:51 +0100

    fixes #2

--- Crawler/modules/adobe_enterprise.py
+++ Crawler/modules/adobe_enterprise.py
@@ -1,3 +1,4 @@
+from urllib import request
 from bs4 import BeautifulSoup
 import urllib
 import datetime
@@ -20,23 +21,66 @@ full_name = "Adobe Reader Enterprise"
 default_architecture = 'win32'
 app_version = 0
 
+
+################################### MAIN
+def getWebSiteAsRequest(url):
+    """
+    Function to get the website (url).
+
+    @Param url: url from the website to catch.
+    @return: Request object from website with url.
+    """
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
+    try:
+        req = requests.get(url, headers=headers, timeout=10.000)
+    except requests.exceptions.RequestException as e:
+        LOGGER.error(e)
+        req = None
+    return req
+
+def getMonthNameByNumber(month_number):
+    """
+    Function to convert a month number to a 3 letter month name.
+
+    @param month_number: Number of month that should be converted to a 3 letter month name.
+    """
+    month = datetime.datetime(datetime.datetime.now().year, month_number, 1)
+    if month_number == 6 or month_number == 7:
+        month_name = month.strftime("%B").lower()[0:4]
+    else:
+        month_name = month.strftime("%B").lower()[0:3]
+    return month_name
+
 def build_download_link():
     """Generate the url to the download page."""
+    base_url = "https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/continuous/dccontinuous"
     year = str(datetime.datetime.now().year)
     month = datetime.datetime.now()
     month_number = datetime.datetime.now().month
     month_name = ""
-    if month_number == 6 or month_number == 7:
-        month_name = month.strftime("%B").lower()[0:4]
-    else: 
-        month_name = month.strftime("%B").lower()[0:3]
 
-    # return link with month and year 
-    return "https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/continuous/dccontinuous" + month_name + year + ".html"
+    try:
+        request = getWebSiteAsRequest(base_url + getMonthNameByNumber(month_number) + year + ".html")
+    except requests.exceptions.RequestException as e:
+        LOGGER.exception(e)
 
+    # check if
+    if request == None:
+        destination_url = base_url + getMonthNameByNumber(month_number - 1) + year + ".html"
+    else:
+        if request.status_code == 404:
+            destination_url = base_url + getMonthNameByNumber(month_number - 1) + year + ".html"
+        else:
+            # return link with month and year
+            destination_url = base_url + getMonthNameByNumber(month_number) + year + ".html"
+    return destination_url
 
 def getWebSite(download_url):
-    """creating request with custom user agent string"""
+    """
+    creating request with custom user agent string
+
+    @param download_url:
+    """
     response = requests.get(download_url).text
     return BeautifulSoup(response, 'html.parser')
 
@@ -65,16 +109,16 @@ def extract_version(download_website):
     return download_website.h1.text.split(' ')[0]
 
 def run():
-    global downloads  
-    global app_version  
-    
+    global downloads
+    global app_version
+
     website = getWebSite(build_download_link())
 
     app_version = extract_version(website)
-    
+
     # get all tables with downloads of any architecture
     tables = website.find_all('table')
-    
+
     # iterate over tables by architecture
     for table in tables:
         if table['id'] == 'id1': # win32
