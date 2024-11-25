39f5c2c9319b7dbed7ead3f8fc6cd6558eb5595a
Marcel Berkholz <code@berkholz.org>
2024-11-25 21:10:52 +0100

    recode template module for crawler

--- Crawler/modules/_template.py
+++ Crawler/modules/_template.py
@@ -1,36 +1,25 @@
-import urllib.request
-from bs4 import BeautifulSoup
-import urllib
-from datetime import date
-import logging # import lib for LOGGING
-import settings # import global settings
+import urllib.request            # import for catching the website
+from bs4 import BeautifulSoup    # import for parsing the website via beautiful soup 4 (https://www.crummy.com/software/BeautifulSoup/bs4/doc/).
+from datetime import date        # import for last_found in JSON export
+import logging                   # import lib for LOGGING
+import settings                  # import global settings
 
 ################################### VARIABLES
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=settings.LogLevel)
+# logging.basicConfig(level=logging.DEBUG)
 
-download_url = 'https://www.url2download.com/downloads.html'
+download_url = ""
 app_name = "example_app".lower()
 full_name = "Example App"
 default_download = 'win64'
-
-base_url = download_url.split('/')[0] + download_url.split('/')[1] + download_url.split('/')[2]
-app_version = 0
-
+app_version = "0"
 
 ################################### FUNCTIONS
-
-def findPlatformInURL(platform, url):
-    if url.find(platform) > 0 and url.find('.asc') <1 and url.find('sha256') <1:
-        return url
-
-def isBinaryURL(ref, platform_string):
-    return ref['href'].find(platform_string) > 0 and ref['href'].find('.asc') <1 and ref['href'].find('sha256') <1
-
-def getWebSite():
+def getWebSite(url):
     # creating request with custom user agent string
     req = urllib.request.Request(
-            download_url,
+            url,
             data=None,
             headers={
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
@@ -39,7 +28,12 @@ def getWebSite():
     with urllib.request.urlopen(req) as f:
         return BeautifulSoup(f.read().decode('utf-8'), 'html.parser')
 
-def toJSON(d):
+def to_json(d):
+    """
+    Export the JSON.
+
+    @param d: Dictionary with all software informations for the downloads.
+    """
     json_result = {
         "app_name": app_name,
         "full_name": full_name,
@@ -53,30 +47,14 @@ def toJSON(d):
 
 def run():
     downloads = list()
-    #website = getWebSite()
-    # tables = website.table
-    # print(tables)
-    # for a in tables.find_all('a', href=True):
-    #     tmp_platform = ''
-    #     tmp_url_bin = ''
-    #     tmp_url_asc = ''
-    #     tmp_url_sha256 = ''
-    #     if isBinaryURL(a, 'win64'):
-    #         tmp_url_bin = base_url + findPlatformInURL('win64', a['href'])
-    #         app_version = tmp_url_bin.split('-')[1]
-    #         downloads.append({"platform": "win64", "url_bin": tmp_url_bin, "url_asc": tmp_url_bin + ".asc", "url_sha256": tmp_url_bin + ".sha256"})
-    #     elif isBinaryURL(a, 'android'):
-    #         tmp_url_bin = base_url + findPlatformInURL('android', a['href'])
-    #         app_version = tmp_url_bin.split('-')[1]
-    #         downloads.append({"platform": "android", "url_bin": tmp_url_bin, "url_asc": tmp_url_bin + ".asc", "url_sha256": tmp_url_bin + ".sha256"})
-    #     elif isBinaryURL(a, 'tar.gz'):
-    #         # we have to find tar.gz, because it is a generic linux tar.gz package
-    #         tmp_url_bin = base_url + findPlatformInURL('tar.gz', a['href'])
-    #         downloads.append({"platform": "linux", "url_bin": tmp_url_bin, "url_asc": tmp_url_bin + ".asc", "url_sha256": tmp_url_bin + ".sha256"})
-    #         #print(url_base + a['href'])
-    return toJSON(downloads)
+    website = getWebSite(download_url)
+
+    # here you add your code to parse the website and extract links and information
+
+    app = {"app_platform": default_download, "url_bin": download_url, "sig_type": None, "sig_res": None,   "hash_type": None, "hash_res": None, "url_pub_key": None}
+    downloads.append(app)
+
+    return to_json(downloads)
 
 if __name__ == "__main__":
-    import sys
     print(run())
-    #run(sys.argv[1])
