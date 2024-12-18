c98bf8535864c5e2f8f955279ba146dba91974a5
Marcel Berkholz <code@berkholz.org>
2024-12-18 23:38:58 +0100

    remove unused import

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import Db.database
 from Db.database import activate_download_for_latest_found
 from Db.database import reset_download_flag_for_all
 import Downloader.downloader
-from Downloader.verify import import_key_from_url, verify
 import logging # import lib for LOGGING
 import settings # import global settings
 import Verifier.verifier
