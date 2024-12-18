c10be01e53c78cf1efc864041ddd6e5bddc9538e
Marcel Berkholz <code@berkholz.org>
2024-12-18 23:04:11 +0100

    - add timeout for crawler modules - add white list for crawler modules, to only execute these modules

--- settings.py
+++ settings.py
@@ -7,3 +7,9 @@ LOGLEVEL = logging.INFO
 
 cwd_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DOWNLOAD_PATH = os.path.join(cwd_dir, 'downloads/')
+
+crawler_module_whitelist = list()
+# to define a whitelits for modules uncomment the line above and add your modules as list
+# crawler_module_whitelist = ['adobe_enterprise']
+
+CRAWLER_MODULE_REQUEST_TIMEOUT = 30
