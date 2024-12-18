22799571123ab1c69f81d05618c53828e301c6da
Marcel Berkholz <code@berkholz.org>
2024-12-18 23:30:44 +0100

    - change name of variable module_path to UPPERCASE - comment out unused import - add logging - add whitelist handling of modules from global settings

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,6 +1,6 @@
 import os
 import glob
-from importlib import import_module
+# from importlib import import_module
 import importlib
 import logging
 import settings
@@ -12,7 +12,7 @@ logging.basicConfig(level=settings.LOGLEVEL)
 all_list = list()
 json_list = list()
 
-module_path = "modules"
+MODULE_PATH = "modules"
 
 
 ################################### FUNCTIONS
@@ -21,7 +21,7 @@ def get_applications():
     Get all application by crawling all modules.
     """
     # iterate over all mpython files in directory crawler_configuration.module_path
-    for f in glob.glob(os.path.dirname(__file__) + "/" + module_path + "/*.py"):
+    for f in glob.glob(os.path.dirname(__file__) + "/" + MODULE_PATH + "/*.py"):
         if os.path.isfile(f) and not os.path.basename(f).startswith('_'):
             all_list.append(os.path.basename(f)[:-3])
 
@@ -29,16 +29,19 @@ def get_applications():
 
     # iterate over all modules
     for mymodule in __all__:
-        LOGGER.info("checking {module} for downloads.".format(module=mymodule))
-        # import module
-        if __name__ == '__main__':
-            # we call the crawler.py directly, so we import without the __package__
-            mod = importlib.import_module(module_path + "." + mymodule)
+        if mymodule in settings.crawler_module_whitelist or len(settings.crawler_module_whitelist) == 0:
+            LOGGER.info("Checking %s for downloads.", mymodule)
+            # import module
+            if __name__ == '__main__':
+                # we call the crawler.py directly, so we import without the __package__
+                mod = importlib.import_module(MODULE_PATH + "." + mymodule)
+            else:
+                # we call the crawler.py from elsewhere, so we import with the __package__
+                mod = importlib.import_module(__package__ + "." + MODULE_PATH + "." + mymodule)
+            # run modules function run()
+            json_list.append(mod.run())
         else:
-            # we call the crawler.py from elsewhere, so we import with the __package__
-            mod = importlib.import_module(__package__ + "." + module_path + "." + mymodule)
-        # run modules function run()
-        json_list.append(mod.run())
+            LOGGER.info("Skipping crawler module %s, because of whitelist.", mymodule)
     return json_list
 
 
