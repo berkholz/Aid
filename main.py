b8689b45dcd8606d4c5b47d81f7c5750c796254d
Marcel Berkholz <code@berkholz.org>
2024-12-18 23:39:32 +0100

    relocate function reset_download_flag_for_all()

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ applications = Crawler.crawler.getApplications("Crawler")
 Db.database.init_db()
 Db.database.append_software(applications)
 
-# activate all software for download which was found latest
-activate_download_for_latest_found()
-
 # reset all download flags
 # reset_download_flag_for_all()
+
+# activate all software for download which was found latest
+activate_download_for_latest_found()
 
 #get software list which have to be downloaded
 software2download = Db.database.get_url_bin_of_software_to_download()
